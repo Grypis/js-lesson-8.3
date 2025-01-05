bb661b0ccce2b71f1f06896115bca8897afcd6f2
grypis <grypisnecros@gmail.com>
2025-01-05 22:53:09 +0200

    asd

---
+++ js/script.js
@@ -0,0 +1,28 @@
+//! ============================================================
+/* import carList from './data/car.js';
+
+function carTemplate({ name, img, price, description }) {
+  const item = document.createElement('li');
+  const title = document.createElement('h2');
+  title.textContent = name;
+  const picture = document.createElement('img');
+  picture.src = img;
+  picture.alt = name;
+  picture.width = 360;
+  picture.height = 300;
+  const dscr = document.createElement('p');
+  dscr.textContent = description;
+  const priceEl = document.createElement('p');
+  priceEl.textContent = price;
+
+  item.append(picture, title, priceEl, dscr);
+
+  return item;
+}
+
+const elements = carList.map(carTemplate);
+const markup = document.querySelector('.js-goods');
+markup.append(...elements);
+ */
+
+//! ============================================================
